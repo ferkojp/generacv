24049f1d253bc435342dd9e8a22a032b499bf4fb
Fernando Jp <ferko300@gmail.com>
2017-02-17 21:24:40 -0600

    get ip

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php 
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+echo $ip;
+ ?>
 <!DOCTYPE html>
 <html lang="es">
     <head>
